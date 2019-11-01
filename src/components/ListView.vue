377e40a8843b6750110ba016a0c2eb15d30cb6dd
subbiah <subbiah_chandramouli@apple.com>
2019-11-01 10:34:06 -0700

    pilot

---
+++ src/components/ListView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="ListView">
+    <div v-for="(value, key) in list" v-bind:key="key">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class ListView extends Vue {
+  @Prop({ default: [], required: true }) private list!: String[];
+}
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="Home">
+    <ListView :list="this.list" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import ListView from "@/components/ListView.vue";
 
-export default {
-  name: "home",
+@Component({
   components: {
-    HelloWorld
+    ListView
   }
-};
+})
+export default class Home extends Vue {
+  private list = ["list1", "list2", "list3"];
+}
 </script>
+
+<style scoped lang="scss"></style>
